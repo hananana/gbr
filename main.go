857902069b79ba726c7973196ce8f25962b70a8d
hananana <hananana.vimmer@gmail.com>
2017-04-18 18:59:44 +0900

    :hammer: wip

--- main.go
+++ main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"github.com/urfave/cli"
-	"gopkg.in/src-d/go-git.v4"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "hoge"
+	app.Name = "gbr"
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("hoge")
+		if !checkGit() {
+			println("not git dir")
+		}
+
+		checkLocalBranches()
+		// 		checkRemoteBranches()
 		return nil
 	}
 
 	app.Run(os.Args)
+}
+
+func checkGit() bool {
+
+	dir, _ := os.Getwd()
+	println(string(dir))
+	return false
+}
+
+func checkLocalBranches() {
+	println("check local branches")
+	out, _ := exec.Command("git", "branch").Output()
+	result := string(out)
+	slice := strings.Split(result, "\r")
+	for _, x := range slice {
+		println(x)
+	}
+}
+
+func checkRemoteBranches() {
+	println("check remote branches")
+	out, _ := exec.Command("git", "branch", "-a").Output()
+	result := string(out)
+	slice := strings.Split(result, "\r")
+	for _, x := range slice {
+		println(x)
+	}
 }
